bb426a113a8fddfc57ac3e590ffa6d94e1c021f4
Mathan raj <mathan.raj43@gmail.com>
2021-05-14 20:06:33 +0530

    Update git workflow

--- deploy.sh
+++ deploy.sh
@@ -23,24 +23,17 @@ cd public
 MESSAGE="rebuilding site $(date)"
 git add .
 
-git config --global user.email "mathan.raj43@gmail.com"
-git config --global user.name "Mathan"
-printf "added git user config"
-git commit -m "$MESSAGE"
-printf "commited"
-git push origin "${GITHUB_BRANCH}"
-printf "pushed"
-
-# if [[ -z "${GITHUB_TOKEN}" || -z "${GITHUB_REPO}" ]]; then
-#   git config --global user.email "ci@github"
-#   git config --global user.name "GitHub Actions CI"
-#   git commit -m "$MESSAGE"
-#   git push origin "${GITHUB_BRANCH}"
-# else
-#   git config --global user.email "ci@github"
-#   git config --global user.name "GitHub Actions CI"
+if [[ -z "${GITHUB_TOKEN}" || -z "${GITHUB_REPO}" ]]; then
+  printf "\033[0;32mcoming inside if...\033[0m\n"
+  git commit -m "$MESSAGE"
+  printf "\033[0;32mpush error if ...\033[0m\n"
+  git push origin "${GITHUB_BRANCH}"
+else
+  printf "\033[0;32mcoming inside else...\033[0m\n"
+  git config --global user.email "ci@github"
+  git config --global user.name "GitHub Actions CI"
   
-#   git commit -m "$MESSAGE"
-  
-#   git push --quiet "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPO}.git" "${GITHUB_BRANCH}"
-# fi
+  git commit -m "$MESSAGE"
+  printf "\033[0;32mpush error else...\033[0m\n"
+  git push --quiet "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPO}.git" "${GITHUB_BRANCH}"
+fi
