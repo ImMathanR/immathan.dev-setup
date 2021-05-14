33d158100a5589ed93f8ebabebaaeb4047d9b5d8
Mathan raj <mathan.raj43@gmail.com>
2021-05-14 19:17:59 +0530

    Added github actions

---
+++ deploy.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+GITHUB_TOKEN="${1}"
+GITHUB_REPO="krossovochkin/krossovochkin.github.io"
+GITHUB_BRANCH="master"
+
+printf "\033[0;32mClean up public folder...\033[0m\n"
+cd public
+git checkout ${GITHUB_BRANCH}
+git pull origin ${GITHUB_BRANCH}
+shopt -s extglob
+rm -rf -- !(CNAME|.git|.|..)
+cd ..
+
+printf "\033[0;32mBuilding website...\033[0m\n"
+hugo
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+cd public
+
+MESSAGE="rebuilding site $(date)"
+git add .
+
+if [[ -z "${GITHUB_TOKEN}" || -z "${GITHUB_REPO}" ]]; then
+  git commit -m "$MESSAGE"
+  git push origin "${GITHUB_BRANCH}"
+else
+  git config --global user.email "ci@github"
+  git config --global user.name "GitHub Actions CI"
+  
+  git commit -m "$MESSAGE"
+  
+  git push --quiet "https://${GITHUB_TOKEN}:x-oauth-basic@github.com/${GITHUB_REPO}.git" "${GITHUB_BRANCH}"
+fi
